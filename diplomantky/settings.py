5d7c6711ee82a4cf688d1fbad7d9e53200ac5a62
agajdosi <andreas.gajdosik@gmail.com>
2022-09-12 17:43:08 +0200

    Commit the rest

--- diplomantky/settings.py
+++ diplomantky/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
+    'students',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'diplomantky.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR.joinpath('templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- students/views.py
+++ students/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
+
+def profiles(request):
+    profiles = Profile.objects.all()
+    return render(request, 'profiles.html', context={'profiles': profiles})
+
+def profile(request):
+    return render(request, 'profile.html')
