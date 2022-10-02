9f1c3bedfeba99e142703882fb7115eaea2313d6
agajdosi <andreas.gajdosik@gmail.com>
2022-10-03 00:21:49 +0200

    Redirect portfolio-edit back to portfolio view

--- students/urls.py
+++ students/urls.py
@@ -5,5 +5,5 @@ from .views import profile, profiles, profile_edit
 urlpatterns = [
   path('', profiles),
   path('<str:first_name>-<str:last_name>/', profile),
-  path('<str:first_name>-<str:last_name>/edit', profile_edit),
+  path('<str:first_name>-<str:last_name>/edit/', profile_edit),
 ]
