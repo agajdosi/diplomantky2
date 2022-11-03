1d21b9966d5aaecae6b701b6bc4b5a3e88414d81
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2022-11-03 16:51:05 +0100

    Add exhibition models

--- students/models.py
+++ students/models.py
@@ -15,6 +15,15 @@ class Profile(models.Model):
     return f'{self.user} ({self.user.first_name} {self.user.last_name})'
 
 
+class Exhibition(models.Model):
+  title = models.CharField(max_length=120)
+  url = models.CharField(max_length=120)
+  artists = models.ManyToManyField(User)
+  description = tinymce_models.HTMLField()
+  
+  def __str__(self):
+    return f'{self.title} - {self.url}'
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
   if created:
