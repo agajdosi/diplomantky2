937dd50f165e72760832d5c154e22f3d7d7d7910
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2022-11-12 00:52:13 +0100

    Better formatting of file paths

--- diplomantky/settings.py
+++ diplomantky/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'diplomantky.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+          BASE_DIR.joinpath('templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,8 +137,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-] #(os.path.join(BASE_DIR, "static"),)
+  BASE_DIR.joinpath("static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
