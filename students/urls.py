9148c65c488180792e29277b84fce7762dfc4143
Andreas Gajdosik <andreas.gajdosik@gmail.com>
2022-11-19 18:26:32 +0100

    Basic index styling

--- students/urls.py
+++ students/urls.py
@@ -5,8 +5,8 @@ from .views import index, graduates, show_portfolio, edit_portfolio, exhibitions
 
 urlpatterns = [
   path('', index),
-  path('graduates', graduates),
-  path('exhibitions', exhibitions),
+  path('graduates/', graduates),
+  path('exhibitions/', exhibitions),
   path('<str:first_name>-<str:last_name>/', show_portfolio),
   path('<str:first_name>-<str:last_name>/edit/', edit_portfolio),
 ]
